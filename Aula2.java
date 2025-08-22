8610be52cf1713c4c4948423f616800aafcedd5d
R-Reichenbach <rafareichenbach@gmail.com>
2025-08-21 21:19:53 -0300

    aula2-poo

---
+++ Aula2.java
@@ -0,0 +1,20 @@
+package AulaJava;
+
+class Aula2 {
+
+    public static void main(String[] args){
+        Carro carro = new Carro();
+        carro.acelerar();
+    }
+}
+
+    class Carro{
+
+        String cor;
+        String Marca;
+        String anoFabricacao;
+
+        public void acelerar(){
+            System.out.print("O carro acelerou !");
+        }
+    }
